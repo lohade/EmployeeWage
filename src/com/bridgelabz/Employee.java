163c89045712272c6bb45487a92617e08243e86b
lohade <abhisheklohade8584@gmail.com>
2021-09-21 11:26:04 +0530

    calculate total wage using object method

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -3,50 +3,15 @@ package com.bridgelabz;
 public class Employee {
     public static  final int IS_PART_TIME=1;
     public static final int IS_FULL_TIME=2;
-    private int EMP_RATE_PER_HOURS;
-    private int NUM_OF_WORKING_DAYS;
-    private int MAX_HRS_IN_MONTH;
-
-    public Employee(int EMP_RATE_PER_HOURS,int NUM_OF_WORKING_DAYS,int MAX_HRS_IN_MONTH){
-        this.NUM_OF_WORKING_DAYS=NUM_OF_WORKING_DAYS;
-        this.MAX_HRS_IN_MONTH=MAX_HRS_IN_MONTH;
-        this.EMP_RATE_PER_HOURS=EMP_RATE_PER_HOURS;
-    }
-
-    public int getEMP_RATE_PER_HOURS() {
-        return EMP_RATE_PER_HOURS;
-    }
-
-    public int getNUM_OF_WORKING_DAYS() {
-        return NUM_OF_WORKING_DAYS;
-    }
-
-    public int getMAX_HRS_IN_MONTH() {
-        return MAX_HRS_IN_MONTH;
-    }
-
-    public void setNUM_OF_WORKING_DAYS(int NUM_OF_WORKING_DAYS) {
-        this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
-    }
-
-    public void setMAX_HRS_IN_MONTH(int MAX_HRS_IN_MONTH) {
-        this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
-    }
-
-    public void setEMP_RATE_PER_HOURS(int EMP_RATE_PER_HOURS) {
-        this.EMP_RATE_PER_HOURS = EMP_RATE_PER_HOURS;
-    }
-
-    public static void main(String[] args) {
-        // write your code here
-        Employee employeeWage=new Employee(15,20,100);
 
 
+
+    public static int EmployeeWage(String company,int EMP_RATE_PER_HOURS,int NUM_OF_WORKING_DAYS,int MAX_HRS_IN_MONTH){
         int empHrs;
         int totalWorkingDays=0;
         int totalEmpHrs=0;
 
-        while(totalEmpHrs<=employeeWage.getMAX_HRS_IN_MONTH() && totalWorkingDays< employeeWage.getNUM_OF_WORKING_DAYS())
+        while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays< NUM_OF_WORKING_DAYS)
         {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -65,8 +30,19 @@ public class Employee {
             totalEmpHrs+=empHrs;
             System.out.println("day#: " +totalWorkingDays + "emp hr:" +empHrs);
         }
-        int totalEmpWage=totalEmpHrs * employeeWage.getEMP_RATE_PER_HOURS();
-        System.out.println("total wage:" + totalEmpWage);
+        int totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOURS;
+        System.out.println("company:" +company+" is giving total wage:" +totalEmpWage);
+        return totalEmpWage;
+
+    }
+
+
+
+    public static void main(String[] args) {
+        // write your code here
+        EmployeeWage("Atos",20,20,100);
+        EmployeeWage("techM",15,15,25);
+
     }
 }
 
